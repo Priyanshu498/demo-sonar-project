f078ddf4ba11b595cd466e627ee465917668675a
Priyanshu Yadav <priyanshu.yadav@opstree.com>
2026-03-16 20:55:53 +0530

    fix sonar issues

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-def greet(name: str) -> str:
-    """
-    Simple greeting function
-    """
-    return f"Hello, {name}!"
-
-
-def main():
-    user = "Developer"
-    message = greet(user)
-    print(message)
-
-
-if __name__ == "__main__":
-    main()
+password = "123456"
+print(password)
